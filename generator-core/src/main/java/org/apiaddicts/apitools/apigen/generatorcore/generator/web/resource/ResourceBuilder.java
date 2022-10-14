dc99670db2281d7acbea2ad766eac62776301088
Adrian Palanques <adrian.palanques@cloudappi.net>
2022-10-14 12:52:10 +0200

    Remove deprecated code

--- generator-core/src/main/java/org/apiaddicts/apitools/apigen/generatorcore/generator/web/resource/ResourceBuilder.java
+++
@@ -1,8 +0,0 @@
-package org.apiaddicts.apitools.apigen.generatorcore.generator.web.resource;
-
-import org.apiaddicts.apitools.apigen.generatorcore.generator.common.AbstractClassBuilder;
-
-@Deprecated
-public abstract class ResourceBuilder extends AbstractClassBuilder {
-
-}
