e4bb0f8358fccbc3e961141b2cbe8d086ea26fab
Adrian Palanques <adrian.palanques@cloudappi.net>
2022-11-24 11:35:28 +0100

    Fix sql script

--- apigen-examples/simple-id-models/data.sql
+++ apigen-examples/simple-id-models/data.sql
@@ -5,9 +5,9 @@ insert into owners(id, name) values ('1', 'Owner 1');
 insert into owners(id, name) values ('2', 'Owner 2');
 insert into owners(id, name) values ('3', 'Owner 3');
 
-insert into pets(name, owner_parent_tag_id) values ('Dog', '1', null, 1);
-insert into pets(name, owner_parent_tag_id) values ('Cat', '1', 1, 1);
-insert into pets(name, owner_parent_id, tag_id) values ('Potato', '1', 2, 2);
+insert into pets(name, owner_id, parent_id, tag_id) values ('Dog', '1', null, 1);
+insert into pets(name, owner_id, parent_id, tag_id) values ('Cat', '1', 1, 1);
+insert into pets(name, owner_id, parent_id, tag_id) values ('Potato', '1', 2, 2);
 
 insert into pet_tags(pet_id, tag_id) values (1, 1);
 insert into pet_tags(pet_id, tag_id) values (1, 2);
